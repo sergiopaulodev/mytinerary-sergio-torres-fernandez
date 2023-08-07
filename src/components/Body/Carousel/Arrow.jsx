6090c3bb00ddbd9b80814a398d2b132d8f60ac88
sergiopaulodev <101745880+sergiopaulodev@users.noreply.github.com>
2023-08-07 18:31:11 -0300

    States (#3)
    * app
    
    * props
    
    * states

--- src/components/Body/Carousel/Arrow.jsx
+++ src/components/Body/Carousel/Arrow.jsx
@@ -1,13 +1,17 @@
 
 
-export default function Arrow({direction}) {
+
+export default function Arrow({direction, onClick}) {
+
   return (
     <>
          <svg 
         xmlns="http://www.w3.org/2000/svg" 
         fill="none" viewBox="0 0 24 24" 
         strokeWidth={1.5} stroke="currentColor" 
-        className="hidden md:flex md:self-center text-gray-500 w-[5%] h-[40px]" 
+
+        className="hidden sm:flex md:self-center text-gray-500 md:w-1/5 cursor-pointer"
+        onClick={onClick} 
         >
           <path strokeLinecap="round" strokeLinejoin="round" d={direction} />
       </svg> 
--- src/components/Body/Carousel/Carousel.jsx
+++ src/components/Body/Carousel/Carousel.jsx
@@ -1,22 +1,59 @@
-import Arrow from "./Arrow"
-import Card from "./Card/Card";
 
+import Arrow from "./Arrow";
+import Card from "./Card/Card";
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
 export default function Carousel({data}) {
+
+const [counterFrom, setCounterFrom] = useState(0)
+const [counterTo, setCounterTo] = useState(4)
+
+function next() {
+    //cuando se pasa de data.length entonces ahí reinicia los contadores
+
+    if (counterTo >= data.length){
+    setCounterFrom( 0 );
+    setCounterTo( 4 )
+
+    }else{
+    setCounterFrom( counterFrom + 4 );
+    setCounterTo( counterTo + 4 )
+    }
+    console.log(counterFrom)
+    console.log(counterTo);
+}
+
+function previous() {
+    if (counterFrom <= 0){
+        setCounterFrom(data.length - 4 );
+        setCounterTo( data.length )
+    }else{
+        setCounterFrom(counterFrom - 4)
+        setCounterTo(counterTo - 4)
+    }
+}
+
+
 
   return (
     <>
-        <div className="flex w-1/2 items-center justify-center">
+    {/* { () ? (que pasa si es true) : (que pasa si es false)} */}
+        <div className="flex flex-wrap md:flex-nowrap lg:w-1/2 items-center justify-center lg:justify-between gap-2">
             <Arrow direction="M15.75 19.5L8.25 12l7.5-7.5" 
-            className="inline-flex" />
-            {data.slice(0,2).map(
+            className="inline-flex" onClick={previous} />
+            
+                {data.slice(counterFrom,counterTo).map(
                 item => <Card
                             key={item.city} 
                             src={item.photo} 
                             city={item.city}
                         />
                 )}
+
+            
             <Arrow direction="M8.25 4.5l7.5 7.5-7.5 7.5"
-                className="inline-flex"/>
+                className="inline-flex" onClick={next} />
         </div>
       
     </>
