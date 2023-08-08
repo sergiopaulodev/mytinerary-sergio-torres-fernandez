49444c709a9e4e49bda6dd0f2e5c72c4c6b23a01
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-08-07 23:12:43 -0300

    logo-image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <>
       <div className="flex justify-between md:items-center lg:mx-20 mx-10 mt-4 md:mb-72 mb-16">
-        <Logo src={ "https://st2.depositphotos.com/1768926/10934/v/450/depositphotos_109347560-stock-illustration-water-wave-logo-template.jpg" } name={"My Tinerary"} />
+        <Logo src={ 'https://media.istockphoto.com/vectors/travel-agency-tourism-app-and-trips-logo-adventure-tours-icon-and-vector-id980663202?k=6&m=980663202&s=170667a&w=0&h=y2cYWhEQPCsFqueBaCMSVQuV2N4jkf-u6KMTSIPmdnU=' } name={"My Tinerary"} />
         <HamMenu />
         <NavBar />
       </div>
--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,10 +1,9 @@
 export default function Logo({ src, name }) {
   
-    const urlLogo = 'https://media.istockphoto.com/vectors/travel-agency-tourism-app-and-trips-logo-adventure-tours-icon-and-vector-id980663202?k=6&m=980663202&s=170667a&w=0&h=y2cYWhEQPCsFqueBaCMSVQuV2N4jkf-u6KMTSIPmdnU='
     return (
     <>
         <div className="flex w-3/4">
-            <div className=" flex items-center bg-cover h-44 opacity" style={{ backgroundImage: `url(${urlLogo})`}}>
+            <div className=" flex items-center bg-cover h-44" style={{ backgroundImage: `url(${src})`}}>
                 <h2 className="flex xl:text-4xl text-3xl font-bold text-[#1C1C1C]" >{name}</h2>
             </div>
             {/* <img src={ src } alt="my tinirery logo" className="w-24 h-auto" />  */}
