c4eb155048d295eb7e1be117ca47323389a09eaf
sergiopaulodev <101745880+sergiopaulodev@users.noreply.github.com>
2023-08-07 23:18:15 -0300

    States (#4)
    * app
    
    * props
    
    * states
    
    * logo-image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,8 @@ export default function Header() {
     <>
       <div className="flex justify-between md:items-center lg:mx-20 mx-10 mt-4 md:mb-72 mb-16">
 
-        <Logo src={ "https://st2.depositphotos.com/1768926/10934/v/450/depositphotos_109347560-stock-illustration-water-wave-logo-template.jpg" } name={"My Tinerary"} />
+        <Logo src={ 'https://media.istockphoto.com/vectors/travel-agency-tourism-app-and-trips-logo-adventure-tours-icon-and-vector-id980663202?k=6&m=980663202&s=170667a&w=0&h=y2cYWhEQPCsFqueBaCMSVQuV2N4jkf-u6KMTSIPmdnU=' } name={"My Tinerary"} />
+
         <HamMenu />
         <NavBar />
       </div>
