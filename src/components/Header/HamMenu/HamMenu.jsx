73ae2b83c806590603bdb101316313e3edf8f4fa
sergiopaulodev <101745880+sergiopaulodev@users.noreply.github.com>
2023-08-08 00:13:22 -0300

    Effects (#5)
    * app
    
    * props
    
    * states
    
    * logo-image
    
    * effects

--- src/components/Header/HamMenu/HamMenu.jsx
+++ src/components/Header/HamMenu/HamMenu.jsx
@@ -9,10 +9,6 @@ export default function HamMenu() {
     const [isChecked, setChecked] = useState(false)
 
 
-    return (
-    <>
-        <div className="flex flex-col justify-between items-end p-0 md:hidden">
-
             
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 flex" 
               onClick={()=>{setChecked(!isChecked)}}
@@ -20,7 +16,6 @@ export default function HamMenu() {
             </svg>
   
               {isChecked ?
-
 
                 <div className="flex flex-col justify-between gap-6 mt-8 items-end ">
                     <LinkHome />
