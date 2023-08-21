8f49a49e3a1795453bcab3ec3706ff0b199e0032
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-08-21 18:25:15 -0300

    data-api

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import CardCities from "../components/Body/Carousel/Card/CardCities";
+import axios from "axios";
+import apiUrl from '../../apiUrl'
 
 export default function Cities() {
+
+    const [data, setData] = useState([])
    
-  return (
+    useEffect(
+
+        () =>{
+            axios(apiUrl + 'cities')
+                // .then(res => console.log(res.data.response))
+                .then(res => setData(res.data.response))
+                .catch(err => console.log(err))
+            },
+        []
+    )
+
+    
+return (
+
     // <div className="flex flex-col items-center w-full min-h-screen">
     //     <h1 className="flex justify-center items-start text-6xl text-[#1C1C1C] uppercase" >404 Error</h1>
     //     <p className="flex justify-center items-start text-3xl text-[#1C1C1C] uppercase">Page not found</p>
     // </div>
+    
+    
     <>
+
+
         <label htmlFor="" className="flex justify-center items-center mt-20">
            <div className="flex justify-start w-1/3 items-center border-2 px-2 rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500">
@@ -17,18 +39,17 @@ export default function Cities() {
            </div>
 
         </label>
-        <div className="flex gap-8 justify-center mt-20 mx-14 mb-20">
-            <CardCities />
-            <CardCities />
-            <CardCities />
-            <CardCities />
-        </div>
-        <div className="flex gap-8 justify-center mt-20 mx-14 mb-20">
-            <CardCities />
-            <CardCities />
-            <CardCities />
-            <CardCities />
+
+        <div className="flex flex-wrap">
+        {data.map(each => <CardCities
+                key={each._id}
+                src={each.photo}
+                city={each.city}
+            /> )}    
+        
+            
         </div>
     </>
-  );
+  )
+
 }
