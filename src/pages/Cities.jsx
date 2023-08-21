94d251bfa1fa53f63cdfeac056b5b8aaa1f3ccb3
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-08-21 01:24:22 -0300

    footer-fixed

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -9,7 +9,7 @@ export default function Cities() {
     // </div>
     <>
         <label htmlFor="" className="flex justify-center items-center mt-20">
-           <div className="flex justify-start items-center border-2 px-2 rounded-lg">
+           <div className="flex justify-start w-1/3 items-center border-2 px-2 rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
