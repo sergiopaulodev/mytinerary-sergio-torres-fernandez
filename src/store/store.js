a5e52dcbd951570e7bea53cb8dfdb8bcdad0aaff
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-08-27 15:47:54 -0300

    checkpoint-redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import city_reducer from "./reducers/cities";
-import allCities_reducer from "./reducers/allCities";
-import cityDetails_reducer from "./reducers/cityDetails";
+// import allCities_reducer from "./reducers/allCities";
+// import cityDetails_reducer from "./reducers/cityDetails";
 
  export default configureStore({
     reducer: {
         cities: city_reducer, 
-                allCities_reducer, 
-                cityDetails_reducer
+                // allCities_reducer, 
+                // cityDetails_reducer
         // itineraries:{ itineraries:[]},
         // users:{ users: []}
     }
