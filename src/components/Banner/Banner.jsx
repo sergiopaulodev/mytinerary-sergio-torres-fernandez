cb5ad1a3d65f6049bfaf3965fdaf2e01fad9a035
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-09-01 18:41:30 -0300

    bg-fixed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,8 @@ export default function Banner({src}) {
     src = 'https://images.pexels.com/photos/731217/pexels-photo-731217.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200'
   return (
     <>
-        <div className="bg-left-bottom bg-no-repeat bg-cover min-h-[50vh] p-8" style={{backgroundImage:`url(${src})`}}>
-            <div className="flex justify-center items-end">
+        <div className="bg-fixed bg-bottom bg-no-repeat bg-cover min-h-[50vh] p-8" style={{backgroundImage:`url(${src})`}}>
+            <div className="flex justify-center pt-[4rem]">
                 <div className="flex flex-col items-center justify-center gap-4">
                     <h2 className='text-4xl text-white  font-bold bg-slate-800 rounded-md opacity-80 p-2'>Cities</h2>
                     <p className='text-2xl text-white font-semibold italic text-center mb-4 bg-slate-800 rounded-md opacity-80 p-2'>Collection of the most beautiful places and experience</p>
--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -4,7 +4,7 @@ import { Link as Anchor} from 'react-router-dom'
 export default function CityDetails( {city, smalldescription, photo}) {
   
   return (
-    <div className="flex flex-col justify-center bg-no-repeat bg-cover min-w-screen min-h-screen"  style={{backgroundImage: `url(${photo})`}}>
+    <div className="flex flex-col justify-center bg-no-repeat bg-fixed bg-cover min-w-screen min-h-screen"  style={{backgroundImage: `url(${photo})`}}>
         <div className='flex justify-center'>
             <div className="flex w-1/2 rounded-3xl bg-slate-50 opacity-80 shadow-lg p-8">
                 <div className="flex flex-col items-center justify-start gap-8">
@@ -18,8 +18,9 @@ export default function CityDetails( {city, smalldescription, photo}) {
                         hover:text-white
                     ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                         </svg>
+
                     </Anchor>
                     </div>
                     <p className="text-lg italic font-semibold text-center w-[70%]">{smalldescription}</p>
