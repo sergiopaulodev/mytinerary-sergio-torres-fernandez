3248726908ffba269e191a837acb016338fdb17f
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-09-01 18:46:01 -0300

    deleted-files

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cities_reducer from "./reducers/cities";
 import itinerary_reducer from "./reducers/itineraries";
-import users_reducer from "./reducers/users";
 
 
  export default configureStore({
     reducer: {
         cities: cities_reducer,
         itineraries: itinerary_reducer,
-        users: users_reducer
         //recurso: recurso_reducer
 
     }
